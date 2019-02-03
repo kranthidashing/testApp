3795abed3aac30df0336932ad46d34456cb2fa2c
kranthikumar <kranthi.k@anblicks.com>
2019-02-04 02:35:12 +0530

    hj

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,12 @@ app.get("/api/test", function(req, res) {
     res.send("Hello World");
 });
 
+
+app.get("/api/users", function(req, res) {
+    res.send("[Kranthi,Chandu,Abhilash,Yash,Roop]");
+});
+
 }
+
 
 module.exports = appRouter;
